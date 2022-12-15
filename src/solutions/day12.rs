9df9238959ca21023fe4e878808d2312c16307eb
tbreslein <tommy.breslein@protonmail.com>
2022-12-15 16:04:47 +0100

    redid day12 with dijkstra; test succeds, but submission fails

--- src/solutions/day12.rs
+++ src/solutions/day12.rs
@@ -1,36 +1,59 @@
-use std::collections::HashMap;
-use std::iter::zip;
+use std::collections::{BinaryHeap, HashSet};
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 struct Node {
-    elevation: i32,
-    cost: i32,
+    elevation: u8,
     is_end: bool,
+    cost: i32,
 }
 
-fn parse(data: &str) -> Vec<Vec<Node>> {
-    let mut lettermap = HashMap::new();
-    for (k, v) in zip('a'..='z', 1..) {
-        lettermap.insert(k, v);
+type Pos = (usize, usize);
+
+#[derive(Debug)]
+struct Edge {
+    node: Pos,
+    cost: i32,
+}
+
+impl Ord for Edge {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.cost.cmp(&self.cost)
     }
+}
+
+impl PartialOrd for Edge {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Edge {
+    fn eq(&self, other: &Self) -> bool {
+        self.cost.eq(&other.cost)
+    }
+}
+
+impl Eq for Edge {}
+
+fn parse(data: &str) -> Vec<Vec<Node>> {
     data.lines()
         .map(|line| {
             line.chars()
                 .map(|c| match c {
                     'S' => Node {
-                        elevation: 0,
-                        cost: 0,
+                        elevation: 'a' as u8,
                         is_end: false,
+                        cost: 0,
                     },
                     'E' => Node {
-                        elevation: i32::MAX,
-                        cost: i32::MAX,
+                        elevation: 'z' as u8 + 1,
                         is_end: true,
-                    },
-                    letter @ 'a'..='z' => Node {
-                        elevation: lettermap.get(&letter).unwrap().clone(),
                         cost: i32::MAX,
+                    },
+                    'a'..='z' => Node {
+                        elevation: c as u8,
                         is_end: false,
+                        cost: i32::MAX,
                     },
                     _ => panic!("wtf"),
                 })
@@ -39,94 +62,65 @@ fn parse(data: &str) -> Vec<Vec<Node>> {
         .collect()
 }
 
-pub fn solve_p1(data: &str) -> i32 {
-    let mut heightmap = parse(data);
-    let mut j = 0;
-    let mut i = 0;
-    // find starting point
-    'outer: for (j_start, col) in heightmap.iter().enumerate() {
-        for (i_start, x) in col.iter().enumerate() {
-            if let Node { elevation: 0, .. } = x {
-                j = j_start;
-                i = i_start;
-                break 'outer;
-            }
-        }
+fn update_neighbors((j, i): &(usize, usize), map: &Vec<Vec<Node>>, neighbors: &mut Vec<Pos>) {
+    neighbors.clear();
+    if *j > 0 && map[*j - 1][*i].elevation - 1 <= map[*j][*i].elevation {
+        neighbors.push((*j - 1, *i));
     }
-    let result = explore(&mut heightmap, j, i);
-    return result.0;
+    if *i > 0 && map[*j][*i - 1].elevation - 1 <= map[*j][*i].elevation {
+        neighbors.push((*j, *i - 1));
+    }
+    if *j < map.len() - 1 && map[*j + 1][*i].elevation - 1 <= map[*j][*i].elevation {
+        neighbors.push((*j + 1, *i));
+    }
+    if *i < map[0].len() - 1 && map[*j][*i + 1].elevation - 1 <= map[*j][*i].elevation {
+        neighbors.push((*j, *i + 1));
+    }
 }
 
-fn explore(heightmap: &mut Vec<Vec<Node>>, j: usize, i: usize) -> (i32, bool) {
-    let num_cols = heightmap.len();
-    let num_rows = heightmap[0].len();
-
-    // check if E nearby in this order: north, south, west, east
-    if (j > 0 && heightmap[j - 1][i].is_end)
-        || (j < num_cols - 1 && heightmap[j + 1][i].is_end)
-        || (i > 0 && heightmap[j][i - 1].is_end)
-        || (i < num_rows - 1 && heightmap[j][i + 1].is_end)
-    {
-        return (heightmap[j][i].cost + 1, true);
-    }
-
-    // explore north
-    if j > 0 {
-        let j_test = j - 1;
-        let i_test = i;
-        if heightmap[j_test][i_test].cost == i32::MAX
-            && heightmap[j_test][i_test].elevation - heightmap[j][i].elevation <= 1
-        {
-            heightmap[j_test][i_test].cost = heightmap[j][i].cost + 1;
-            if let x @ (_, true) = explore(heightmap, j_test, i_test) {
-                return x;
+pub fn solve_p1(data: &str) -> i32 {
+    let mut map = parse(data);
+    // find starting point
+    let (mut j, mut i) = (0, 0);
+    for jstart in 0..map.len() {
+        for istart in 0..map[0].len() {
+            if map[jstart][istart].cost == 0 {
+                (j, i) = (jstart, istart);
             }
         }
     }
 
-    // explore south
-    if j < num_cols - 1 {
-        let j_test = j + 1;
-        let i_test = i;
-        if heightmap[j_test][i_test].cost == i32::MAX
-            && heightmap[j_test][i_test].elevation - heightmap[j][i].elevation <= 1
-        {
-            heightmap[j_test][i_test].cost = heightmap[j][i].cost + 1;
-            if let x @ (_, true) = explore(heightmap, j_test, i_test) {
-                return x;
+    let mut visited = HashSet::new();
+    let mut queue = BinaryHeap::new();
+    queue.push(Edge {
+        node: (j, i),
+        cost: 0,
+    });
+
+    let mut neighbors = Vec::with_capacity(3); // any node never has more than 3 unvisited neighbors
+    while let Some(Edge { node, cost }) = queue.pop() {
+        if !visited.insert(node) {
+            continue;
+        }
+
+        update_neighbors(&node, &map, &mut neighbors);
+        for neighbor in neighbors.iter() {
+            let new_cost = cost + 1;
+            if map[neighbor.0][neighbor.1].is_end {
+                return new_cost;
+            }
+
+            if new_cost < map[neighbor.0][neighbor.1].cost {
+                map[neighbor.0][neighbor.1].cost = new_cost;
+                queue.push(Edge {
+                    node: neighbor.clone(),
+                    cost: new_cost,
+                });
             }
         }
     }
 
-    // explore west
-    if i > 0 {
-        let j_test = j;
-        let i_test = i - 1;
-        if heightmap[j_test][i_test].cost == i32::MAX
-            && heightmap[j_test][i_test].elevation - heightmap[j][i].elevation <= 1
-        {
-            heightmap[j_test][i_test].cost = heightmap[j][i].cost + 1;
-            if let x @ (_, true) = explore(heightmap, j_test, i_test) {
-                return x;
-            }
-        }
-    }
-
-    // explore east
-    if i < num_rows - 1 {
-        let j_test = j;
-        let i_test = i + 1;
-        if heightmap[j_test][i_test].cost == i32::MAX
-            && heightmap[j_test][i_test].elevation - heightmap[j][i].elevation <= 1
-        {
-            heightmap[j_test][i_test].cost = heightmap[j][i].cost + 1;
-            if let x @ (_, true) = explore(heightmap, j_test, i_test) {
-                return x;
-            }
-        }
-    }
-
-    return (0, false);
+    return 0;
 }
 
 pub fn solve_p2(data: &str) -> usize {
